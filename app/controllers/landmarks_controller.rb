9a7461a6537b556ca106e088ad6312bd3ca17d55
mkenane <morkenan@hotmail.com>
2018-04-06 11:11:58 -0400

    Friday Second Commit

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,13 +5,31 @@ class LandmarksController < ApplicationController
     erb '/landmarks/index'.to_sym
   end
 
+  get '/landmarks/new' do
+    @figures = Figure.all
+    erb 'landmarks/new'.to_sym
+  end
+
   get '/landmarks/:id' do #gets a certain landmark
     @landmark = Landmark.find(params[:id])
     erb '/landmarks/show'.to_sym
   end
 
-  get '/landmarks/new' do
-    erb 'landmarks/new'.to_sym
+
+
+  post '/landmarks' do
+    puts params 
+    # @landmark = Landmark.create(name: params[:name], year_completed: params[:year_completed])
+    redirect to '/landmarks'
   end
-  
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @figures = Figure.all
+    erb '/landmarks/edit'.to_sym
+  end
+
+
+
+
 end
